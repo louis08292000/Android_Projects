9f30e18586e7e367754d73b35f9ccc91d0c265da
louis08292000 <louis08292000@gmail.com>
2016-02-14 10:33:57 +0800

    repesent th dir
    new a java code

---
+++ RepesentMkdir.java
@@ -0,0 +1,30 @@
+import java.io.*;
+public class RepesentMkdir{
+	public static void main(String[] args){
+		File  f = new File("C:/Users/ÂE¼y"); 
+		tree(f,0);
+	}
+
+	public static void tree(File f, int lev){
+		int currectlev;
+		File[] chidf = f.listFiles();
+		for(int i=0;i<chidf.length;i++){
+			level(lev);
+			if(chidf[i].isDirectory()){
+				System.out.println(chidf[i].getName());
+				currectlev = lev+1;
+				tree(chidf[i],currectlev);
+			}else{
+				System.out.println(chidf[i].getName());
+			}
+				
+		}
+	}
+	
+	public static void level(int lev){
+		for(int i=0;i<lev;i++){
+			System.out.print("   ");
+		}
+	}
+
+}
